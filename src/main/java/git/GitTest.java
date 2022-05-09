267b1d2a1533bafa6b68e1a6a8ebc84727479bc4
Syk <Syk@qq.com>
2022-05-09 20:54:52 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * @author SYK
+ * @version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git~");
+    }
+}
